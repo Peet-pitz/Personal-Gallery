bca0caf81d76d9fc4bb1bc4d879fe9bbb1c1424a
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-02 11:09:23 +0300

    add search url

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
+    url(r'^search/',views.search_results,name='search_results'),
 ]
