191794cd46f16d62cb66351a7a6387459f644942
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-02 11:08:53 +0300

    add archives of url

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.photos_of_day,name='photosToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
 ]
