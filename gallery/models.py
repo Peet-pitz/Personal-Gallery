0ceb09ba6edd13798b1c8c107214a3ce8a4c773a
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-04 19:43:15 +0300

    add class photographer

--- gallery/models.py
+++ gallery/models.py
@@ -1,18 +1,18 @@
-# from django.db import models
-# import datetime as dt
+from django.db import models
+import datetime as dt
 
 
 
-# class Photographer(models.Model):
-#     first_name = models.CharField(max_length = 30)
-#     last_name = models.CharField(max_length = 30)
-#     email = models.EmailField()
+class Photographer(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
 
-#     def __str__(self):
-#         return self.first_name
+    def __str__(self):
+        return self.first_name
     
-#     def save_editor(self):
-#         self.save()
+    def save_editor(self):
+        self.save()
 
 # class tags(models.Model):
 #     name = models.CharField(max_length = 30)
